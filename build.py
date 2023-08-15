bbcb5e5f3e676383aaf4888f4f6086ae526ce119
Don Naro <dnaro@redhat.com>
2023-08-15 11:30:00 +0100

    separate clean and main functions

--- build.py
+++ build.py
@@ -6,27 +6,37 @@ from yaml import Loader, load
 
 import sass
 
+
 def data():
     yaml = Path("./data").glob("*.yaml")
-    data_dict = {file_path.stem: load(file_path.read_text(), Loader=Loader) for file_path in yaml}
+    data_dict = {
+        file_path.stem: load(file_path.read_text(), Loader=Loader) for file_path in yaml
+    }
 
     return data_dict
 
-buildpath = Path('output')
 
-if buildpath.exists() and buildpath.is_dir():
-    shutil.rmtree(buildpath)
+def clean_buildpath(output_dir):
+    if output_dir.exists() and output_dir.is_dir():
+        shutil.rmtree(output_dir)
 
-if __name__ == "__main__":
+
+def main():
+    buildpath = Path("output")
+    clean_buildpath(buildpath)
 
     site = Site.make_site()
-    site.outpath=buildpath
-    site.contexts=[(".*.html", data)]
+    site.outpath = buildpath
+    site.contexts = [(".*.html", data)]
     # disable automatic reloading
     site.render(use_reloader=False)
 
     # copy the static folder
-    shutil.copytree('static', buildpath / 'static')
+    shutil.copytree("static", buildpath / "static")
 
     # compile sass to css
-    sass.compile(dirname=('sass', buildpath / 'static/css'))
+    sass.compile(dirname=("sass", buildpath / "static/css"))
+
+
+if __name__ == "__main__":
+    main()
