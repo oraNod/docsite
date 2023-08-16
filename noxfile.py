118159d0ca4cd79d86ac7aa0a5cb20c9c0b91043
Don Naro <dnaro@redhat.com>
2023-08-16 16:57:40 +0100

    Update noxfile.py
    Co-authored-by: Sviatoslav Sydorenko <wk.cvs.github@sydorenko.org.ua>

--- noxfile.py
+++ noxfile.py
@@ -3,9 +3,7 @@ import nox
 @nox.session
 def build(session: nox.Session):
     session.install(
-    "-r",
-    "requirements.in",
-    "-c",
-    "requirements.txt",
+      "-r", "requirements.in",
+      "-c", "requirements.txt",
     )
     session.run("python", "build.py")
